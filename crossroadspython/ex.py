35bd147b4fa92d79cacb2b89f9507e3d6424f089
ajmalbinnizam <ajmalnizam369@gmail.com>
2020-06-20 23:45:03 +0530

    day 1

--- crossroadspython/ex.py
+++ crossroadspython/ex.py
@@ -1,4 +1,9 @@
+a=int(input("your number"))
+b=input()
+c=10
 
+sum1=a+int(b)
+print("result is" , sum1)
 
+print("result is "+str(sum1))
 
-print("hi")
---
+++ crossroadspython/p.py
@@ -0,0 +1,12 @@
+print("* * * *")
+
+print("*     *")
+
+print("*     *")
+
+print("* * * *")
+
+dt="123456"
+print(len(dt))
+print(dt.upper())
+print(dt[2:-1])
